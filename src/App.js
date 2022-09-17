300c7d67726f2cca9b398341ff82e04f62a6418c
Yen Nguyen <ngkimyen7901@gmail.com>
2022-09-17 11:13:22 +0700

    add layout

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Layout from './components/global/Layout'
 
 const App = () => {
   return (
-      <div className="max-w-[400px] min-h-[700px] m-auto bg-white p-5">
-        Components here !
-      </div>
+      <Layout>
+        App
+      </Layout>
   )
 }
 
