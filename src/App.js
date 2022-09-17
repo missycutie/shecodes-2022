e5d742e42b9526c37da18daf086a6a9d8a44c09c
Yen Nguyen <ngkimyen7901@gmail.com>
2022-09-17 16:28:33 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Layout from './components/global/Layout'
 const App = () => {
   return (
       <Layout>
         <div className="">Component</div>
       </Layout>
+      
   )
 }
 
