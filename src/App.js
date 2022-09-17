36fe2ddfc480d6b4f7c43cccd94e412856f0adaf
Yen Nguyen <ngkimyen7901@gmail.com>
2022-09-17 11:03:31 +0700

    set layout for project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Layout from './components/global/Layout'
 
 const App = () => {
   return (
-    <div className="text-3xl">App</div>
+      <div className="max-w-[400px] min-h-[700px] m-auto bg-white p-5">
+        Components here !
+      </div>
   )
 }
 
